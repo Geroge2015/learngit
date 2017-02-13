0617124c6a2ff8ef2db394bc024b808754490203
George2015 <sunnyfrank2008@126.com>
2017-02-13 17:24:41 +0800

    add base fragment and listener.

---
+++ dev/mylocker/deyLocker/app/src/main/java/cover/frank/cm/mylocker/IFragmentStatusListener.java
@@ -0,0 +1,9 @@
+package cover.frank.cm.mylocker;
+
+/**
+ * Created by George on 2017/2/13.
+ */
+
+public interface IFragmentStatusListener {
+    void onFragmentViewCreated();
+}
---
+++ dev/mylocker/deyLocker/app/src/main/java/cover/frank/cm/mylocker/SettingBaseFragment.java
@@ -0,0 +1,34 @@
+package cover.frank.cm.mylocker;
+
+import android.app.Fragment;
+
+/**
+ * Created by George on 2017/2/13.
+ */
+
+public class SettingBaseFragment extends Fragment {
+
+    private boolean mIsAdded;
+
+    public SettingBaseFragment() {
+        mIsAdded = false;
+    }
+
+    public void setAdded(boolean b) {
+        mIsAdded = b;
+    }
+
+    public boolean getAdded() {
+        return mIsAdded;
+    }
+
+    public void onCallBack() {
+
+    }
+
+    public void setFragmentStatusListener(IFragmentStatusListener listener) {
+
+    }
+
+
+}
